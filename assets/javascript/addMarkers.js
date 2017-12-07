31da6beb1e72c9415a24998f2475dbc042c7cf1c
andrejarboe <andrejarboe@gmail.com>
2017-12-07 12:12:19 -0500

    added lodging to icon list

--- assets/javascript/addMarkers.js
+++ assets/javascript/addMarkers.js
@@ -8,17 +8,24 @@ function addMarkers(lat, lng, name, category) {
   } else if (category === 'food') {
     imageURL = './assets/images/icons/restaurant.svg';
   } else if (category === 'sports') {
-    if (name.toLowerCase().search('gym') > -1|| name.toLowerCase().search('fitness') > -1) {
+    if (name.toLowerCase().search('gym') > -1 || name.toLowerCase().search('fitness') > -1) {
       imageURL = './assets/images/icons/gym.svg';
-    }else{
-      imageURL = './assets/images/icons/stadium.svg';      
+    } else {
+      imageURL = './assets/images/icons/stadium.svg'
     }
-  } else {
+  } else if (category === 'nightlife') {
+    imageURL = './assets/images/icons/night-club.svg'
+  } else if (category === 'shopping') {
+    imageURL = './assets/images/icons/shopping-mall.svg'
+  } else if(category === 'lodging'){
+    imageURL = './assets/images/icons/lodging.svg';
+  }else {
     imageURL = './assets/images/icons/map-pin.svg';
   }
 
   //make image
   var image = {
+    fillColor: '#00CCBB',
     url: imageURL,
     scaledSize: new google.maps.Size(25, 25), // scaled size
     origin: new google.maps.Point(0, 0), // origin
